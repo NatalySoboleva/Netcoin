a1949f82394fbb66f0763f32e9cc7d26b3cb5f9a
Soboleva Natalia <nsoboleva1979@rambler.ru>
2021-04-08 21:07:03 +0300

    Внесены правки

--- public/homePage.js
+++ public/homePage.js
@@ -27,22 +27,71 @@ function updateRates() {
         }
     });
 }
-updateRates();
+setTimeout(updateRates);
 setInterval(updateRates, 60000);
 
-moneyManager.addMoneyCallback = function (response) {
-    ApiConnector.addMoney((response) => {
-        if (response.success) {
-            ProfileWidget.showProfile;
-            moneyManager.setMessage('Баланс успешно пополнен!');
-        } else {
-            moneyManager.setMessage(response.error);
+moneyManager.addMoneyCallback = data => {
+    ApiConnector.addMoney(data, response => {
+        if (!response.success) {
+            moneyManager.setMessage(response.success, `Средства добавлены не были!`);
         }
-    });
+        ProfileWidget.showProfile(response.data);
+        moneyManager.setMessage(response.success, `Сумма добавлена!`);
+    })
+}
+
+moneyManager.conversionMoneyCallback = data => {
+    ApiConnector.convertMoney(data, response => {
+        if (!response.success) {
+            moneyManager.setMessage(response.success, `Конвертация не проведена!`);
+        }
+        ProfileWidget.showProfile(response.data);
+        moneyManager.setMessage(response.success, `Конвертация успешно проведена!`);
+    })
+}
+
+moneyManager.sendMoneyCallback = data => {
+    ApiConnector.transferMoney(data, response => {
+        if (!response.success) {
+            moneyManager.setMessage(response.success, `Сумма не переведена!`);
+        }
+        ProfileWidget.showProfile(response.data);
+        moneyManager.setMessage(response.success, `Сумма успешно переведена!`);
+    })
 }
 
 function updateFavorites(data) {
-    favoritesWidget.clearTable(data);
+    favoritesWidget.clearTable();
     favoritesWidget.fillTable(data);
     moneyManager.updateUsersList(data);
+}
+
+ApiConnector.getFavorites(response => {
+    if (response.success) {
+        updateFavorites(response.data);
+    }
+});
+
+favoritesWidget.addUserCallback = data => {
+    ApiConnector.addUserToFavorites(data, response => {
+        if (response.success) {
+            updateFavorites(data);
+            favoritesWidget.setMessage(response.success, `Пользователь добавлен!`);
+            return;
+        }
+        favoritesWidget.setMessage(response.success, `Пользователь не добавлен!`);
+        return;
+    })
+}
+
+favoritesWidget.removeUserCallback = data => {
+    ApiConnector.removeUserFromFavorites(data, response => {
+        if (response.success) {
+            updateFavorites(data);
+            favoritesWidget.setMessage(response.success, `Пользователь удален!`);
+            return;
+        }
+        favoritesWidget.setMessage(response.success, `Пользователь не удален!`);
+        return;
+    })
 }
